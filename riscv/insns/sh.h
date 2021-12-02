b30dd25762e8d1a6a285ce006b547a26b4afd7d2
xingyuli9961 <xingyuli9961@berkeley.edu>
2021-12-02 10:24:36 +0000

    update store tracing logic

--- riscv/insns/sh.h
+++ riscv/insns/sh.h
@@ -1,12 +1,12 @@
+MMU.store_uint16(RS1 + insn.s_imm(), RS2);
+
 // Xingyu: Start
 #include <iostream>
 
-std::cout << "SotreTrace:" << " SH " << "newdata: " << std::hex << RS2 << " addr:"  << " 0x" << std::hex << (RS1 + insn.i_imm());
+std::cout << "StoreTrace:" << " addr:" << " 0x" << std::hex << (RS1 + insn.i_imm());
 for (size_t i = 0; i < 16; i++) {
   std::cout << " 0x" << std::hex << MMU.load_uint32(((RS1 + insn.i_imm()) & (~63)) + 4 * i);
 }
 std::cout << std::endl;
 
 // Xingyu: End
-
-MMU.store_uint16(RS1 + insn.s_imm(), RS2);
